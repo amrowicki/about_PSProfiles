e67ef52950e527c58684999abcc44178c1c0bf86
amrowicki <amrowicki@users.noreply.github.com>
2018-07-15 21:50:50 +0200

    Work in progress...

---
+++ 1-TheShellVSTheHost.ps1
@@ -0,0 +1,14 @@
+break #so that it won't break anything
+
+#The Shell vs. The Host
+
+Start-Process powershell_ise.exe
+Start-Process powershell.exe
+Start-Process pwsh.exe
+
+Get-Host
+$Host
+$PSVersionTable
+
+Write-Host
+[System.Console]::WriteLine()
---
+++ 2-Basics.ps1
@@ -0,0 +1,7 @@
+break #so that it won't break anything
+
+$profile
+$profile.AllUsersAllHosts
+$profile.AllUsersCurrentHost
+$profile.CurrentUserAllHosts
+$profile.CurrentUserCurrentHost
